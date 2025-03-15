dd602137f9096e2f82894d9b55b0446d8783c630
pafeuu <pawel.mejerr@gmail.com>
2025-03-15 10:40:15 +0100

    storage block natures aura tags

--- kubejs/server_scripts/Tags.js
+++ kubejs/server_scripts/Tags.js
@@ -109,6 +109,9 @@ ServerEvents.tags('item', event => {
   event.add('forge:ingots/tainted_gold','naturesaura:tainted_gold')
   event.add("forge:ingots/infused_iron","naturesaura:infused_iron")
   event.add("forge:storage_blocks/infused_iron","naturesaura:infused_iron_block")
+  event.add("forge:storage_blocks/tainted_gold","naturesaura:tainted_gold_block")
+  event.add("forge:storage_blocks/sky","naturesaura:sky_ingot_block")
+  event.add("forge:storage_blocks/depth","naturesaura:depth_ingot_block")
 
   event.remove('forge:ingots/steel', 'immersiveengineering:ingot_steel')
   event.remove('forge:nuggets/steel', 'immersiveengineering:nugget_steel')
