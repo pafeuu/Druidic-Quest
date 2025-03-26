0477afc0f29b27f508d60eb81ef193f3d659c330
pafeuu <pawel.mejerr@gmail.com>
2025-03-26 20:12:57 +0100

    change names of essences from different mods

---
+++ kubejs/client_scripts/names.js
@@ -0,0 +1,13 @@
+ClientEvents.lang('en_us', event => {
+
+    event.renameItem('irons_spellbooks:arcane_essence', 'Arcane Dust')
+    event.renameItem('irons_spellbooks:cinder_essence', 'Cinder Dust')
+    event.renameItem('tombstone:essence_of_undeath', 'Ectoplasm')
+    event.renameItem('twilightforest:borer_essence', 'Borer Dust')
+    event.renameItem('alexsmobs:poison_bottle', 'Poison Bottle')
+    
+
+    event.renameItem('enigmaticlegacy:infinimeal', 'Eternal Fertilizer')
+    event.renameItem('enigmaticlegacy:evil_essence', 'Aspect of Evil')
+
+})
