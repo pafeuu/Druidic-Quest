dca261c1caf0ea99795c7351bae8fd0163931111
pafeuu <pawel.mejerr@gmail.com>
2025-06-14 11:18:34 +0200

    animal spawner critters

---
+++ kubejs/server_scripts/animal_spawner.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes((event)=>{
+
+    event.recipes.naturesaura.animal_spawner("ars_nouveau:whirlisprig",["naturesaura:birth_spirit","#minecraft:saplings","kubejs:nature_essence"],50000,60)
+    event.recipes.naturesaura.animal_spawner("ars_nouveau:drygmy",["naturesaura:birth_spirit","ars_nouveau:wilden_horn","kubejs:nature_essence"],50000,60)
+    event.recipes.naturesaura.animal_spawner("ars_nouveau:starbuncle",["naturesaura:birth_spirit","#forge:nuggets/gold","kubejs:nature_essence"],50000,60)
+    event.recipes.naturesaura.animal_spawner("allay",["naturesaura:birth_spirit","amethyst_shard","kubejs:nature_essence"],50000,60)
+})
